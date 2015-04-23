077c2c1aa0a74fef9313de4ca051829bdaed0905
Gunawan <gunawan.my.id@gmail.com>
2015-04-23 14:25:43 +0800

    Update Laporan1.java
    silahkan direvisi dikit yah...^_^

--- src/chun/Laporan1.java
+++ src/chun/Laporan1.java
@@ -2,26 +2,26 @@ package chun;
 
 public class Laporan1 {
 	
-static String cun= "Welcome Laporan 1 \n";
+static String cun= "Welcome Laporan 1 \n"; //berikan penjelasan
 	
 public static void main(String[] args) {
         
-		System.out.println(cun);
+		System.out.println(cun); //berikan penjelasan
        
-        biodata();
-        aritmatika();
+        biodata(); //berikan penjelasan
+        aritmatika(); //berikan penjelasan
 
     }
 	
-	public static void aritmatika()
+	public static void aritmatika() //berikan penjelasan
 	{
 		//buat variabel untuk menampung data
-		 int a=3,b=4,c=1;
+		 int a=3,b=4,c=1; 
 	        double d=0;
 	       
 	        System.out.println("\n=== Operasi Aritmatika ===\n");
 	        //tambah
-	        c=a+b;
+	        c=a+b; //berikan penjelasan
 	        System.out.println("Hasil dari "+a+"+"+b+"="+c);
 	        //kurang
 	        c=a-b;
@@ -32,15 +32,15 @@ public static void main(String[] args) {
 	        //bagi
 	        d=(double)a/b;
 	        System.out.println("Hasil dari "+a+"/"+b+"="+d);
-	        //modulus
+	        //modulus //berikan penjelasan lengkap
 	        c=a%b;
 	        System.out.println("Hasil dari "+a+"mod"+b+"="+c);
 	        
 	}
 	
-	public static void biodata()
+	public static void biodata() //berikan penjelasan
 	{
-		String name ="Rasimah";
+		String name ="Rasimah"; //berikan penjelasan
 		String nim ="60200112028";
 		System.out.println("Nama "+name+ "\nNim  "+nim);
 	}
